33e7f3a9c1d2e683e9bf460f67dd9d9068db4af6
RST2024 <rtanwani2024@gmail.com>
2024-06-27 22:14:11 -0700

    msg

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ def main():
     Code Your Program here
     ########################################
     """
+    number = 0
+    while True:
+        try: 
+            number = int(input('Enter a number  '))
+            if number > 0 and number < 100:
+                break
+            else: 
+                print ('Enter a correct number')
+        except ValueError:
+            print ('Only enter a number')
+            
 
     print(number)
 
